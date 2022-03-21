fdc3e30e7b5b950ed7202770705bee76b52a08a7
James Lacen <jlacen2355@hotmail.com>
2022-03-21 12:09:50 -0500

    Addin my first three classes in the project mobile, Monitor and Mouse!!!

---
+++ src/day1/Mobile.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class Mobile {
+
+    //states
+
+    //Network
+    //Launch
+    //Platform
+    //Memory
+    //camera
+    //Sound
+    //Comms
+    //Features
+    //Battery
+    //Misc
+    //Test
+
+    //Behaviours
+
+    //Voice Call
+    //Video Call
+    //Text Message
+
+}
---
+++ src/day1/Monitor.java
@@ -0,0 +1,28 @@
+package day1;
+
+public class Monitor {
+
+    //states
+
+    //Resolution
+    //Size
+    //Aspect
+    //Ratio
+    //PanelType
+    //RefreshRate
+    //ResponseTime
+    //SynchronisationTechnology
+    //ViewingAngles
+    //InputConnectors
+    // Curvature
+    //Brightness
+    //HDR
+    //Contrast
+    //Color Space
+
+    //Behaviours
+
+    //TransmitVideo
+    //TransmitSound
+
+}
