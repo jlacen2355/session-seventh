0f8a8aa8e247d87f57282517c6f71e0f73fa644b
James Lacen <jlacen2355@hotmail.com>
2022-03-21 12:27:35 -0500

    Addin my first three classes in the project mobile, Monitor and Mouse!!!

---
+++ src/day1/Mouse.java
@@ -0,0 +1,26 @@
+package day1;
+
+public class Mouse {
+
+    //states
+
+    //Sensor
+    //DPI
+    //PollRate
+    //TrackingSpeed
+    //Buildtype
+    // BuildSize
+    //WiredorWireless
+    //ProgrammableButtons
+    //Weight
+    //Liftoffdistance
+    //AngleSnapping
+
+    //Behaviours
+
+    //Selectaction
+    //Dragfiles
+    //DisplayInfo
+
+
+}
