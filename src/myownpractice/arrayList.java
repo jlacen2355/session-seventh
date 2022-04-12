528ba13799f88654db0b39faae26a52fbea08289
James Lacen <jlacen2355@hotmail.com>
2022-04-12 08:44:34 -0500

    Practice of arrayList by myself

---
+++ src/myownpractice/arrayList.java
@@ -0,0 +1,18 @@
+package myownpractice;
+
+import java.util.ArrayList;
+
+public class arrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> carriers = new ArrayList<String>();
+        carriers.add("BST");
+        carriers.add("ATT");
+        carriers.add("VZW");
+        carriers.add("SPR");
+        System.out.println(carriers);
+
+    }
+
+}
