8d7c09b1be5025ecf415fdd3fb073c016f9b31fc
James Lacen <jlacen2355@hotmail.com>
2022-04-20 11:39:36 -0500

    Adding Day15 files and assigment(Assigment file fix- Update)

--- src/day15/descendantarrayassigment/DescendantArray.java
+++ src/day15/descendantarrayassigment/DescendantArray.java
@@ -8,8 +8,9 @@ public class DescendantArray {
     public static void main(String[] args) {
 
         // Example Array for demostration
-//        int[] counter = {5,9,6,1,7,4,3,8,2,};
-        Integer counter[] = {5,9,6,1,7,4,3,8,2,};
+        int[] counter = {5,9,6,1,7,4,3,8,2,};
+        System.out.println("Example Array for demostration : " + Arrays.toString(counter) );
+
 
         //first is the sorted Array (Increasing)
         Arrays.sort(counter);
@@ -19,16 +20,19 @@ public class DescendantArray {
             System.out.println(count);
         }
 
+
         //Second is the reversed Array (descendant)
-        Arrays.sort(counter, Collections.reverseOrder());
         System.out.println("The numbers are sorted in reversed (decreasing way)...");
+        int counterLength = counter.length;
+        int[] counter2 = new int[counterLength];
 
-        for(int count : counter){
-            System.out.println(count);
+        for(int i = 0; i < counterLength; i++){
+
+            int decrease = counterLength - 1 - i;
+            counter2[i] = counter[decrease];
+            System.out.println(counter2[i]);
+
         }
-
-
-
 
 
 
