e79c97c62db5f4dee41caf7e5110f847fdc84425
James Lacen <jlacen2355@hotmail.com>
2022-05-05 09:06:47 -0500

    Adding Day25 files

---
+++ src/day25/GenericPractice.java
@@ -0,0 +1,20 @@
+package day25;
+
+import java.util.logging.Logger;
+
+public class GenericPractice {
+
+    public static void main(String[] args) {
+        GenericPair<String, String> genericPair = new GenericPair<String, String>("name", "James");
+
+        String genericPairValue = genericPair.getValue();
+
+        GenericPair<String, Integer>  genericPair1 = new GenericPair<String, Integer>("age", 30);
+
+        int genericPair1Value = genericPair1.getValue();
+
+        GenericPair<Integer, Integer>  genericPair2 = new GenericPair<Integer, Integer>(2, 30);
+
+        Logger.getGlobal().info(String.valueOf(genericPair));
+    }
+}
